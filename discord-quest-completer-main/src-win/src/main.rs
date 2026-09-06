8555512a48b1648c806e97ef4c05767fbd3cc749
markterense <specifieryt@gmail.com>
2026-09-07 00:13:56 +0600

    Delete discord-quest-completer-main directory

--- discord-quest-completer-main/src-win/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Ce fichier est vide car toute la logique est dans main.cpp
-// Le point d'entrée réel est défini par /ENTRY:mainEntryPoint dans build.rs
-#![no_main]
-#![no_std]
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
--- discord-quest-completer-main/src/utils/random-string.ts
+++
@@ -1,5 +0,0 @@
-import { nanoid } from 'nanoid'
-
-export const randomString = (length: number = 16): string => {
-    return nanoid(length)
-}
